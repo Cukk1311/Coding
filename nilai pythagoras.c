b1ab449e3941f904d9b9e06068522115f56dc8f8
Cukk1311 <90719417+Cukk1311@users.noreply.github.com>
2022-04-06 12:00:54 +0700

    Create nilai pythagoras.c

---
+++ nilai pythagoras.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+	int a,b,c,d;
+	scanf ("%i", &a);
+	scanf ("%i", &b);
+	c = (a*a)+(b*b);
+	d = sqrt(c);
+	printf("n nilai pythagoras dari angka diatas adalah %i", d);
+	getch();
+}
